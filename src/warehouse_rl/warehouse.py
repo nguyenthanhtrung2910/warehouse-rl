bb8dc2f7c477a67bc61bb500844f8e048500096d
snowman <nguyenthanhtrung291099@gmail.com>
2026-03-06 00:51:34 +0300

    Warehouse parameter as input.

--- src/warehouse_rl/warehouse.py
+++ src/warehouse_rl/warehouse.py
@@ -1,6 +1,6 @@
 # from pettingzoo import ParallelEnv
-import json
-import os
+# import json
+# import os
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
@@ -38,32 +38,122 @@ class Warehouse:
     __agents: list[str]
     __graph: nx.DiGraph
 
-    def __init__(self, n_robots: int) -> None:
+    def __init__(
+        self,
+        n_robots: int,
+        n_rows: int,
+        n_columns: int,
+        n_lines: int,
+        n_subrows: int,
+        is_double_line: bool,
+    ) -> None:
         # super().__init__()
         self.__agents = [f"a{i}" for i in range(n_robots)]
-        self.__init_graph()
+        self.__init_graph(n_rows, n_columns, n_lines, n_subrows, is_double_line)
 
-    def __init_graph(self):
+    def __add_edge(self, n1: Node, n2: Node, direction: bool):
+        if direction:
+            self.__graph.add_edge(n1, n2)
+        else:
+            self.__graph.add_edge(n2, n1)
+
+    def __init_graph(
+        self,
+        n_rows: int,
+        n_columns: int,
+        n_lines: int,
+        n_subrows: int,
+        is_double_line: bool,
+    ):
         self.__graph = nx.DiGraph()
-        id_nodes = {}
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        with open(file_dir + "/../../wareshouse.json", "r") as file:
-            warehouse_config = json.load(file)
-        for node_data in warehouse_config["nodes"]:
-            node = Node(**node_data)
-            id_nodes[node.id] = node
-        for edge_data in warehouse_config["edges"]:
-            src_node = id_nodes[edge_data["source"]]
-            tgt_node = id_nodes[edge_data["target"]]
-            self.__graph.add_edge(src_node, tgt_node)
-        for node_data in warehouse_config["lineNodes"]:
-            self.__graph.add_node(LineNode(**node_data))
+        direction: bool = False
+        n_rays: int = 2 if is_double_line else 1
+        for column in range((n_lines + 1) * n_columns):
+            for row in range(n_rows + 1):
+                # double ray in first and last rays
+                if row == n_rows:
+                    for pair in range(0, 2):
+                        self.__add_edge(
+                            Node(
+                                f"{column}.{(n_subrows + n_rays) * row - n_rays + pair + 2}",
+                                column,
+                                (n_subrows + n_rays) * row - n_rays + pair + 2,
+                                False,
+                            ),
+                            Node(
+                                f"{column + 1}.{(n_subrows + n_rays) * row - n_rays + pair + 2}",
+                                column + 1,
+                                (n_subrows + n_rays) * row - n_rays + pair + 2,
+                                False,
+                            ),
+                            direction,
+                        )
+                        direction = not direction
+                elif row == 0:
+                    for pair in range(0, 2):
+                        self.__add_edge(
+                            Node(
+                                f"{column}.{pair}",
+                                column,
+                                pair,
+                                False,
+                            ),
+                            Node(
+                                f"{column + 1}.{pair}",
+                                column + 1,
+                                pair,
+                                False,
+                            ),
+                            direction,
+                        )
+                        direction = not direction
+                # else add number rays based on is_double_line
+                else:
+                    for pair in range(0, n_rays):
+                        self.__add_edge(
+                            Node(
+                                f"{column}.{(n_subrows + n_rays) * row - n_rays + pair + 2}",
+                                column,
+                                (n_subrows + n_rays) * row - n_rays + pair + 2,
+                                False,
+                            ),
+                            Node(
+                                f"{column + 1}.{(n_subrows + n_rays) * row - n_rays + pair + 2}",
+                                column + 1,
+                                (n_subrows + n_rays) * row - n_rays + pair + 2,
+                                False,
+                            ),
+                            direction,
+                        )
+                        direction = not direction
+        for row in range(n_rows):
+            for column in range(n_columns):
+                for innner_row in range(n_subrows):
+                    for innner_column in range(n_lines):
+                        self.__graph.add_node(
+                            LineNode(
+                                f"{column * (n_lines + 1) + innner_column + 1}.{row * (n_subrows + n_rays) + innner_row + 2}",
+                                column * (n_lines + 1) + innner_column + 1,
+                                row * (n_subrows + n_rays) + innner_row + 2,
+                                False,
+                            )
+                        )
+        for n1 in self.__graph.nodes:
+            for n2 in self.__graph.nodes:
+                if (n2.y - n1.y == 1) or (n2.y - n1.y == (n_subrows + 1)):
+                    if n1.x == 0 and n2.x == 0:
+                        self.__add_edge(n1, n2, True)
+                    if (
+                        n1.x == (n_lines + 1) * n_columns
+                        and n2.x == (n_lines + 1) * n_columns
+                    ):
+                        self.__add_edge(n1, n2, False)
 
     def draw(self):
         pos: dict[Node | LineNode, tuple[int, int]] = {
             node: (node.x, node.y) for node in self.__graph.nodes
         }
-        plt.figure(figsize=(20, 12), dpi=100)
+        plt.figure(figsize=(20, 16), dpi=100)
         node_colors = []
         for node in self.__graph.nodes:
             if type(node) is LineNode:
@@ -74,8 +164,8 @@ class Warehouse:
             G=self.__graph,
             pos=pos,
             node_color=node_colors,
-            node_size=50,
-            arrowsize=5,
+            node_size=200,
+            arrowsize=20,
         )
         plt.savefig("graph.png")
 
@@ -93,5 +183,5 @@ class Warehouse:
 
 
 if __name__ == "__main__":
-    env = Warehouse(2)
+    env = Warehouse(2, 2, 3, 5, 5, True)
     env.draw()
